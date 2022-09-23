33e51f2b0ec4ebef472dd28d781bb1398e74681b
krissathvick <114206963+krissathvick@users.noreply.github.com>
2022-09-23 10:51:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,15 @@ const THREE = window.MINDAR.IMAGE.THREE;
 document.addEventListener('DOMContentLoaded', () => {
 
 const myStart = async () => {
-	
+	const video = document.createElement("video");
+	video.setAttribute("src", "./assets/video/sjc.mp4")
+	video.setAttribute("loop", "");
+
+	video.oncanplay = () => {
+		video.play();
+		resolve(video.captureStream());
+	}
+
 	const mindarThree = new window.MINDAR.IMAGE.MindARThree({
 		container: document.body,
 		imageTargetSrc: './assets/target/sjc.mind',
@@ -13,7 +21,8 @@ const myStart = async () => {
 	const {renderer, scene, camera} = mindarThree;
 
 	const myGeometry = new THREE.PlaneGeometry(1, 1);
-	const myMaterial = new THREE.MeshBasicMaterial({color: 0x0000ff, transparent: true, opacity: 0.5});
+	const videoTexture = new THREE.VideoTexture(video);
+	const myMaterial = new THREE.MeshBasicMaterial({map: videoTexture, side: THREE.FrontSide, toneMapped: false });
 	const myPlane = new THREE.Mesh(myGeometry, myMaterial);
 
 	const anchor = mindarThree.addAnchor(0);
